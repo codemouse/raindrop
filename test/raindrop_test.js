8ba6a559552059687bc6c0fb3c707aae6e627e73
Brian Sodano <briansodano@gmail.com>
2015-04-08 04:43:42 -0400

    restored tests

--- test/raindrop_test.js
+++ test/raindrop_test.js
@@ -3,8 +3,46 @@
 var raindrop = require('../lib/raindrop');
 
 //create new raindrop with defaults
-for (var i = 0; i < 100000; i++) {
-  var drop = raindrop();
-  console.log(drop.getRandomCounter())
-}
+var drop = raindrop();
 
+//get Raindrop Object
+console.log (drop);
+
+//set options for serviceTypeId and entityTypeId (0 - 255)
+//set options for machineId (0 - 16777215)
+var opt = {serviceTypeId: 1, entityTypeId: 4, machineId: 478444};
+
+//create new raindrop with options
+drop = raindrop(opt);
+
+//get Raindrop Object
+console.log (drop);
+
+//get Raindrop Object as 16 character string
+console.log (drop.toString());
+
+//get Raindrop Object as 24 character decoded hex string
+console.log (drop.toString(true));
+
+//get Type info (returns Raindrop Object version according to installed Node package)
+console.log("Type: " + drop._type);
+
+//get Timestamp portion up to the second as ISO 8601 date from UTC decoded
+console.log("Timestamp: " + drop.getTimestamp());
+
+//get MachineId decoded
+console.log("Machine Id: " + drop.getMachineId());
+
+//get ServiceId decoded
+console.log("Service Id: " + drop.getServiceId());
+
+//get EntityTypeId decoded
+console.log("Entity Type Id: " + drop.getEntityTypeId());
+
+//get RandomCounter decoded
+console.log("RandomCounter: " + drop.getRandomCounter());
+
+/*for (var i = 0; i < 100000; i++) {
+ var drop = raindrop();
+ console.log(drop.getRandomCounter())
+ }*/
