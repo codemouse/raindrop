62659b68194a9cc744136c059d18ab274d74738a
Brian Sodano <briansodano@gmail.com>
2016-01-10 09:33:42 -0500

    removed coffee and gulp

--- gulpfile.js
+++
@@ -1,4 +0,0 @@
-// Note the new way of requesting CoffeeScript since 1.7.x
-require('coffee-script/register');
-// This bootstraps your Gulp's main file
-require('./gulpfile.coffee');
--- src/config.coffee
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-module.exports =
-  'defaultPid': parseInt(Math.random() * 0xFFFFFF, 10)
-  'defaultSid': 0
-  'defaultEid': 0
